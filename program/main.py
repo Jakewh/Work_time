892a0e7d6ea392a3822335280aec5bda476de9b7
Jakub Kolář <78088608+Jakewh@users.noreply.github.com>
2022-03-07 12:11:57 +0100

    .

--- program/main.py
+++ program/main.py
@@ -1,4 +1,17 @@
 import tkinter as tk
+"""
+__        __         _      _____ _                
+\ \      / /__  _ __| | __ |_   _(_)_ __ ___   ___ 
+ \ \ /\ / / _ \| '__| |/ /   | | | | '_ ` _ \ / _ \
+  \ V  V / (_) | |  |   <    | | | | | | | | |  __/
+   \_/\_/ \___/|_|  |_|\_\   |_| |_|_| |_| |_|\___|V0.1
+"""
 
-window = tk.Tk()
+# vytvoří instanci třídy
+window = tk.Tk()  
+
 greeting = tk.Label(text="Hello, Tkinter")
+greeting.pack()
+
+# metoda hlavního okna mainloop, udržuje okno otevřené
+window.mainloop()
