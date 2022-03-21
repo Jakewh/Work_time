10395a42a5e1a0d257bb0c414621a24723d21f6a
Jakub Kolář <78088608+Jakewh@users.noreply.github.com>
2022-03-21 13:19:33 +0100

    .

--- program/main.py
+++ program/main.py
@@ -16,14 +16,7 @@ __        __         _      _____ _
    \_/\_/ \___/|_|  |_|\_\   |_| |_|_| |_| |_|\___|V0.1
 """
 
-# základ okna ###############################################
-okno = tk.Tk()
-okno.title("Evidence pracovní doby")
-# ikona okna
-ikona = PhotoImage(file="/home/jakub/GitHub/Work_time/program/clock.png")
-okno.iconphoto(True, ikona)
-
-# menubar ###############################################
+# seznam funkcí ###############################################
 def openfile():
     """Položka "Otevřít" v menu "Soubor" """
     with open("vykaz.csv") as myfile:
@@ -49,6 +42,70 @@ def info_menu():
     Label(info_okno, text="Jakub Kolář\nkolarkuba@gmail.com\n2022").grid(row=2, column=0, sticky="we", pady=5, padx=5)
     close_button = Button(info_okno,text = "Zavřít",command=lambda:info_okno.destroy()).grid(row=3, column=0, pady=5)
 
+def prevod_hodin():
+    """Převod času na desítkovou soustavu"""
+    od = Od_vstup.get()
+    do = Do_vstup.get()
+    rozbor_od = od.split(":")
+    rozbor_do = do.split(":")
+    prevod_od = (int(rozbor_od[0]) + float(rozbor_od[1])/60)
+    prevod_do = (int(rozbor_do[0]) + float(rozbor_do[1])/60)
+    celkem_h += (prevod_do-prevod_od)
+
+#def soucet_hodin():
+    """Součet všech hodin v taulce"""
+    summa = 0 
+    summa += float(Hodin_vstup.get())
+    celkem_h.delete(0,"end")
+    celkem_h.insert(0,summa)
+
+def added():
+    """ Automatický výpočet hodin za den"""
+    try :
+        h.set((int(Od_vstup.get().replace("", "")) + int(Do_vstup.get().replace("", ""))))
+    except :
+        pass
+    Input_frame.after(1, added)
+    return
+
+def vlozit_zaznam():
+    """ nastavení vyčítaní vkládacích polí"""
+    global count
+    global celkem_h
+    #soucet_hodin(),
+    tabulka.insert(
+    parent="",
+    index="end",
+    iid = count,
+    text="",
+    values=(
+    Datum_vstup.get(),
+    Od_vstup.get(),
+    Do_vstup.get(),
+    Hodin_vstup.get(),
+    Misto_vstup.get(),
+    Poznamka_vstup.get()))
+    count += 1   
+    Datum_vstup.delete(0,END)
+    Od_vstup.delete(0,END)
+    Do_vstup.delete(0,END)
+    Hodin_vstup.delete(0,END)
+    Misto_vstup.delete(0,END)
+    Poznamka_vstup.delete(0,END),
+    
+def smazat():
+    """ Smazání vybraných dat z tabulky"""
+    selected_item = tabulka.selection()[0]
+    tabulka.delete(selected_item)
+
+# základ okna ###############################################
+okno = tk.Tk()
+okno.title("Evidence pracovní doby")
+# ikona okna
+ikona = PhotoImage(file="/home/jakub/GitHub/Work_time/program/clock.png")
+okno.iconphoto(True, ikona)
+
+# menubar ###############################################
 mb = Menu(okno)
 file_menu = Menu(mb, tearoff=0)
 file_menu.add_command(label="Otevřít poslední", command=openfile)
@@ -65,23 +122,11 @@ jmeno = Entry(okno)
 jmeno.insert(10,"")
 jmeno.grid(row=3, column=0, sticky="w", padx=5)
 
-# Součet ###############################################
+# Součet všech hodin ###############################################
 Label(okno, text="Celkem hodin").grid(row=2, column=0, sticky="e", padx=5)
-celkem_h = 0 # součet všech hodin
 soucet = Entry(okno, width=5)
-soucet.insert(10,celkem_h)
+
 soucet.grid(row=3, column=0, sticky="e", padx=5, pady=5)
-
-def soucet_hodin():
-    """Součet všech "Hodin" v tabulce"""
-    od = "7:30"
-    do = "16:00"
-    rozbor_od = od.split(":")
-    rozbor_do = do.split(":")
-    prevod_od = (int(rozbor_od[0]) + float(rozbor_od[1])/60)
-    prevod_do = (int(rozbor_do[0]) + float(rozbor_do[1])/60)
-    celkem_h += (prevod_do-prevod_od)
-
 
 # tabulka ###############################################
 tabulka = ttk.Treeview(okno)
@@ -109,7 +154,7 @@ data = []
 global count
 count = 1   
 for zaznam in data:      
-    tabulka.insert(parent='', index="end", iid=count, text="", values=(zaznam[0], zaznam[1], zaznam[2], zaznam[3], zaznam[4], zaznam[5]))       
+    tabulka.insert(parent="", index="end", iid=count, text="", values=(zaznam[0], zaznam[1], zaznam[2], zaznam[3], zaznam[4], zaznam[5]))       
     count += 1
 
 # Rámec vkládacích polí ###############################################
@@ -122,22 +167,13 @@ Do = Label(Input_frame,text="Do").grid(row=0,column=2)
 Misto = Label(Input_frame,text="Místo").grid(row=0,column=4)
 Poznamka = Label(Input_frame,text="Poznámka").grid(row=0,column=5)
 # vkládací pole
-def added():
-    """ Automatický výpočet hodin za den"""
-    try :
-        h.set((int(Od_vstup.get().replace("", "")) + int(Do_vstup.get().replace("", ""))))
-    except :
-        pass
-    Input_frame.after(1, added)
-    return
-
 Datum_vstup = DateEntry(Input_frame, width=8)
 Datum_vstup.grid(row=1,column=0)
 Od_vstup = Entry(Input_frame, width=8)
 Od_vstup.grid(row=1,column=1)
 Do_vstup = Entry(Input_frame, width=8)
 Do_vstup.grid(row=1,column=2)
-h = StringVar()
+h = DoubleVar()
 Hodin_vstup = Entry(Input_frame, textvariable=h, width=0)
 Input_frame.after(1,added)
 Misto_vstup = Entry(Input_frame, width=24)
@@ -147,44 +183,13 @@ Poznamka_vstup.grid(row=1,column=5)
 
 # Rámec tlačítek ###############################################
 Button_frame = Frame(okno)
-Button_frame.grid(row=6, column=0)
-
-def vlozit_zaznam():
-    """ nastavení vyčítaní vkládacích polí"""
-    global count
-    tabulka.insert(
-    parent="",
-    index="end",
-    iid = count,
-    text="",
-    values=(
-    Datum_vstup.get(),
-    Od_vstup.get(),
-    Do_vstup.get(),
-    Hodin_vstup.get(),
-    Misto_vstup.get(),
-    Poznamka_vstup.get()))
-    count += 1   
-    Datum_vstup.delete(0,END)
-    Od_vstup.delete(0,END)
-    Do_vstup.delete(0,END)
-    Hodin_vstup.delete(0,END)
-    Misto_vstup.delete(0,END)
-    Poznamka_vstup.delete(0,END)
-    soucet_hodin()
-    
-def smazat():
-    """ Smazání vybraných dat z tabulky"""
-    selected_item = tabulka.selection()[0]
-    tabulka.delete(selected_item)
-         
+Button_frame.grid(row=6, column=0) 
 # button "Vložit záznam"
 butt_plus = PhotoImage(file="/home/jakub/GitHub/Work_time/program/plus.png")
 Input_button = Button(Button_frame, command=vlozit_zaznam, image=butt_plus, relief="flat").grid(row=0, column=0, pady=5)
-
 # button "Smazat záznam"
 butt_minus = PhotoImage(file="/home/jakub/GitHub/Work_time/program/minus.png")
 delete_button = Button(Button_frame, command=smazat, image=butt_minus, relief="flat").grid(row=0, column=1, pady=5)
 
-# metoda hlavního okna mainloop, udržuje okno otevřené
+
 okno.mainloop()
