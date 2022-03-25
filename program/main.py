fc35f0c6397c32bb290be036442dd5db8450a324
Jakub Kolář <78088608+Jakewh@users.noreply.github.com>
2022-03-25 09:44:37 +0100

    místo tabulky -> label

--- program/main.py
+++ program/main.py
@@ -16,7 +16,16 @@ __        __         _      _____ _
    \_/\_/ \___/|_|  |_|\_\   |_| |_|_| |_| |_|\___|V0.1
 """
 
-# seznam funkcí ###############################################
+
+# základ okna ###############################################
+okno = tk.Tk()
+okno.title("Evidence pracovní doby")
+okno.resizable(False, False)
+# ikona okna
+ikona = PhotoImage(file="/home/jakub/GitHub/Work_time/program/clock.png")
+okno.iconphoto(True, ikona)
+
+# menubar ###############################################
 def openfile():
     """Položka "Otevřít" v menu "Soubor" """
     with open("vykaz.csv") as myfile:
@@ -33,164 +42,88 @@ def savefile():
             csvwriter.writerow(row)
     showinfo("Potvrzení", "Uložení proběhlo v pořádku.")
 
-def info_menu():
+def info():
     """ Položka "Info" v menu"""
     info_okno = tk.Toplevel()
     info_okno.title("Info")
     Label(info_okno, image=ikona).grid(row=0, column=0, pady=5)
     Label(info_okno, text="WORK TIME v0.1", font="bold").grid(row=1, column=0, sticky="we", pady=5, padx=5)
     Label(info_okno, text="Jakub Kolář\nkolarkuba@gmail.com\n2022").grid(row=2, column=0, sticky="we", pady=5, padx=5)
-    close_button = Button(info_okno,text = "Zavřít",command=lambda:info_okno.destroy()).grid(row=3, column=0, pady=5)
 
-def prevod_hodin():
-    """Převod času na desítkovou soustavu"""
-    od = Od_vstup.get()
-    do = Do_vstup.get()
-    rozbor_od = od.split(":")
-    rozbor_do = do.split(":")
-    prevod_od = (int(rozbor_od[0]) + float(rozbor_od[1])/60)
-    prevod_do = (int(rozbor_do[0]) + float(rozbor_do[1])/60)
-    celkem_h += (prevod_do-prevod_od)
-
-#def soucet_hodin():
-    """Součet všech hodin v taulce"""
-    summa = 0 
-    summa += float(Hodin_vstup.get())
-    celkem_h.delete(0,"end")
-    celkem_h.insert(0,summa)
-
-def added():
-    """ Automatický výpočet hodin za den"""
-    try :
-        h.set((int(Od_vstup.get().replace("", "")) + int(Do_vstup.get().replace("", ""))))
-    except :
-        pass
-    Input_frame.after(1, added)
-    return
-
-def vlozit_zaznam():
-    """ nastavení vyčítaní vkládacích polí"""
-    global count
-    global celkem_h
-    #soucet_hodin(),
-    tabulka.insert(
-    parent="",
-    index="end",
-    iid = count,
-    text="",
-    values=(
-    Datum_vstup.get(),
-    Od_vstup.get(),
-    Do_vstup.get(),
-    Hodin_vstup.get(),
-    Misto_vstup.get(),
-    Poznamka_vstup.get()))
-    count += 1   
-    Datum_vstup.delete(0,END)
-    Od_vstup.delete(0,END)
-    Do_vstup.delete(0,END)
-    Hodin_vstup.delete(0,END)
-    Misto_vstup.delete(0,END)
-    Poznamka_vstup.delete(0,END),
-    
-def smazat():
-    """ Smazání vybraných dat z tabulky"""
-    selected_item = tabulka.selection()[0]
-    tabulka.delete(selected_item)
-
-# základ okna ###############################################
-okno = tk.Tk()
-okno.title("Evidence pracovní doby")
-okno.resizable(False, False)
-# ikona okna
-ikona = PhotoImage(file="/home/jakub/GitHub/Work_time/program/clock.png")
-okno.iconphoto(True, ikona)
-
-# menubar ###############################################
-mb = Menu(okno)
+mb = Menu(okno, relief="flat")
 file_menu = Menu(mb, tearoff=0)
 file_menu.add_command(label="Otevřít poslední", command=openfile)
 file_menu.add_command(label="Uložit", command=savefile)
 file_menu.add_separator()
+file_menu.add_command(label="Info", command=info)
 file_menu.add_command(label="Zavřít", command=lambda:okno.destroy())
-mb.add_cascade(label="Soubor", menu=file_menu)
-mb.add_command(label="Info", command=info_menu)
+mb.add_cascade(label="≡", menu=file_menu, font=(18))
 okno.config(menu=mb)
 
-# Jméno ###############################################
-Label(okno, text="Jméno").grid(row=2, column=0, sticky="w", padx=5)
-jmeno = Entry(okno)
-jmeno.insert(10,"")
-jmeno.grid(row=3, column=0, sticky="w", padx=5)
+def info():
+    """ Položka "Info" v menu"""
+    info_okno = tk.Toplevel()
+    info_okno.title("Info")
+    Label(info_okno, image=ikona).grid(row=0, column=0, pady=5)
+    Label(info_okno, text="WORK TIME v0.1", font="bold").grid(row=1, column=0, sticky="we", pady=5, padx=5)
+    Label(info_okno, text="Jakub Kolář\nkolarkuba@gmail.com\n2022").grid(row=2, column=0, sticky="we", pady=5, padx=5)
 
-# Součet všech hodin ###############################################
-Label(okno, text="Celkem hodin").grid(row=2, column=0, sticky="e", padx=5)
-soucet = Entry(okno, width=5)
 
-soucet.grid(row=3, column=0, sticky="e", padx=5, pady=5)
+# tabulka dat ###############################################
+radek = 1
+zaznam = 0
+# popisky tabulky
+popisky = Frame(okno)
+popisky.grid(row=1, column=0)
+Datum = Label(popisky,text="Datum", width=8, bg="#BFBFBF").grid(row=0,column=0)
+Od = Label(popisky,text="Od", width=8, bg="#BFBFBF").grid(row=0,column=1)
+Do = Label(popisky,text="Do", width=8, bg="#BFBFBF").grid(row=0,column=2)
+Hodin = Label(popisky,text="Hodin", bg="#BFBFBF").grid(row=0,column=3)
+Misto = Label(popisky,text="Místo", width=24, bg="#BFBFBF").grid(row=0,column=4)
+Poznamka = Label(popisky,text="Poznámka", width=15, bg="#BFBFBF").grid(row=0,column=5)
+smazat = Label(popisky,text="", width=3, bg="#BFBFBF").grid(row=0,column=6)
+nic = Label(popisky,text="Zatím žádný záznam", font=("Helvetica", 8)).grid(row=1, column=2, columnspan=3, pady=20)
 
-# tabulka ###############################################
-tabulka = ttk.Treeview(okno)
-tabulka.grid(row=4, column=0, sticky="w", padx=5)
-# nastavení sloupců
-tabulka["columns"] = ("Datum", "Od", "Do", "Hodin", "Misto", "Poznamka")
-tabulka.column("#0", width=0,  stretch=NO)
-tabulka.column("Datum", anchor=CENTER, width=80)
-tabulka.column("Od", anchor=CENTER, width=80)
-tabulka.column("Do", anchor=CENTER, width=80)
-tabulka.column("Hodin", anchor=CENTER, width=80)
-tabulka.column("Misto", anchor=CENTER, width=150)
-tabulka.column("Poznamka", anchor=CENTER, width=80)
-# nastavení popisků sloupců
-tabulka.heading("#0",text="",anchor=CENTER)
-tabulka.heading("Datum",text="Datum",anchor=CENTER)
-tabulka.heading("Od",text="Od",anchor=CENTER)
-tabulka.heading("Do",text="Do",anchor=CENTER)
-tabulka.heading("Hodin",text="Hodin",anchor=CENTER)
-tabulka.heading("Misto",text="Místo",anchor=CENTER)
-tabulka.heading("Poznamka",text="Poznámka",anchor=CENTER)
-
-# data ###############################################
-data = []
-global count
-count = 1   
-for zaznam in data:      
-    tabulka.insert(parent="", index="end", iid=count, text="", values=(zaznam[0], zaznam[1], zaznam[2], zaznam[3], zaznam[4], zaznam[5]))       
-    count += 1
-
-# Rámec vkládacích polí ###############################################
+# vkládací pole ###############################################
 Input_frame = Frame(okno)
-Input_frame.grid(row=5, column=0)
-# Popisky vkládacích polí
-Datum = Label(Input_frame,text="Datum").grid(row=0,column=0)
-Od = Label(Input_frame,text="Od").grid(row=0,column=1)
-Do = Label(Input_frame,text="Do").grid(row=0,column=2)
-Misto = Label(Input_frame,text="Místo").grid(row=0,column=4)
-Poznamka = Label(Input_frame,text="Poznámka").grid(row=0,column=5)
-# vkládací pole
+Input_frame.grid(row=50, column=0)
+Label(okno,text="vložit nový záznam", font=("Helvetica", 8)).grid(row=49, column=0, sticky="w")
+ttk.Separator(okno, orient='horizontal').grid(row=48, column=0, pady=15, sticky="we")
+
 Datum_vstup = DateEntry(Input_frame, width=8)
 Datum_vstup.grid(row=1,column=0)
 Od_vstup = Entry(Input_frame, width=8)
+Od_vstup.insert(0, "od")
 Od_vstup.grid(row=1,column=1)
 Do_vstup = Entry(Input_frame, width=8)
+Do_vstup.insert(0, "do")
 Do_vstup.grid(row=1,column=2)
-h = DoubleVar()
-Hodin_vstup = Entry(Input_frame, textvariable=h, width=0)
-Input_frame.after(1,added)
 Misto_vstup = Entry(Input_frame, width=24)
+Misto_vstup.insert(0, "místo")
 Misto_vstup.grid(row=1,column=4)
 Poznamka_vstup = Entry(Input_frame, width=15)
+Poznamka_vstup.insert(0, "poznámka")
 Poznamka_vstup.grid(row=1,column=5)
 
 # Rámec tlačítek ###############################################
 Button_frame = Frame(okno)
-Button_frame.grid(row=6, column=0) 
-# button "Vložit záznam"
-butt_plus = PhotoImage(file="/home/jakub/GitHub/Work_time/program/plus.png")
-Input_button = Button(Button_frame, command=vlozit_zaznam, image=butt_plus, relief="flat").grid(row=0, column=0, pady=5)
-# button "Smazat záznam"
-butt_minus = PhotoImage(file="/home/jakub/GitHub/Work_time/program/minus.png")
-delete_button = Button(Button_frame, command=smazat, image=butt_minus, relief="flat").grid(row=0, column=1, pady=5)
+Button_frame.grid(row=51, column=0) 
+# button "+"
+def pridat():
+    global radek
+    global nic
+    Label(popisky, text=Datum_vstup.get()).grid(row=radek, column=0)
+    Label(popisky, text=Od_vstup.get()).grid(row=radek, column=1)
+    Label(popisky, text=Do_vstup.get()).grid(row=radek, column=2)
+    Label(popisky, text="hodin").grid(row=radek, column=3)
+    Label(popisky, text=Misto_vstup.get()).grid(row=radek, column=4)
+    Label(popisky, text=Poznamka_vstup.get()).grid(row=radek, column=5)
+    zaznam += 1
+    radek += 1
+
+Input_button = Button(Button_frame, text="✚", command=pridat, relief="flat", font=(18)).grid(row=0, column=0, pady=5)
+
+
 
 
 okno.mainloop()
